9b3031a125aa75e4a83592b8812f613ad26f5c3b
minj-L <wonder_minji@naver.com>
2025-11-03 19:26:17 +0900

    feat(consoleMessage) : 콘솔 출력용 메시지 작성

---
+++ src/main/java/lotto/common/ConsoleMessage.java
@@ -0,0 +1,10 @@
+package lotto.common;
+
+public class ConsoleMessage {
+    public static final String PAYING_COSTS = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_CNT = "%d개를 구매했습니다.";
+    public static final String INPUT_WINNING_NUMS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUM = "보너스 번호를 입력해 주세요.";
+    public static final String LOTTO_RES = "당첨 통계";
+    public static final String TOTAL_COSTS = "총 수익률은 %.1f%%입니다.";
+}
